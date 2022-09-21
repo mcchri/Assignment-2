f229d0d66a6654403494a55da173d3e1abdfcb76
mcchri <112865223+mcchri@users.noreply.github.com>
2022-09-21 10:59:53 +0100

    Add files via upload

---
+++ sum.js
@@ -0,0 +1,27 @@
+const PI = 3.14;
+const R = 4;
+const H = 10;
+const L = 7;
+const W = 9;
+const B = 5;
+
+
+
+
+let area_circle = PI * R ** 2;
+let circumference_circle = 2*PI*R;
+let area_rectangle = L*W;
+let volume_cylinder = PI*R**2*H;
+let volume_cuboid = L*H*W;
+let area_tri = B*H/2;
+let c_pythogras = B**2+H**2;
+let volume_sphere = R**3*PI;
+
+console.log(area_circle);
+console.log(circumference_circle);
+console.log(area_rectangle);
+console.log(volume_cylinder);
+console.log(volume_cuboid);
+console.log(area_tri);
+console.log(c_pythogras);
+console.log(volume_sphere);
